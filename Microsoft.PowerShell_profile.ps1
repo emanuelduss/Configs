e86a36250435a4409feb6a706f5bfda10b2791e3
Emanuel Duss <me@emanuelduss.ch>
2022-03-03 14:48:48 +0100

    New: PowerShell Profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,46 @@
+# PowerShell Options
+
+Set-PSReadlineOption -EditMode vi
+Set-PSReadlineOption -BellStyle None
+
+# Colors
+
+Set-PSReadLineOption -Colors @{
+  Command            = 'Magenta'
+  Number             = 'DarkGray'
+  Member             = 'DarkGray'
+  Operator           = 'DarkGray'
+  Type               = 'DarkGray'
+  Variable           = 'DarkGreen'
+  Parameter          = 'DarkGreen'
+  ContinuationPrompt = 'DarkGray'
+  Default            = 'DarkGray'
+}
+
+Set-PSReadLineOption -Colors @{
+ # Use a ConsoleColor enum
+ "Error" = [ConsoleColor]::DarkRed
+
+ # 24 bit color escape sequence
+ "String" = "$([char]0x1b)[38;5;100m"
+
+ # RGB value
+ "Command" = "#8181f7"
+}
+
+
+# Prompt
+
+function prompt
+{
+  Write-Host "PS " -nonewline -ForegroundColor "DarkYellow"
+  Write-Host "$env:USERNAME@$env:COMPUTERNAME " -ForegroundColor "Green" -NoNewLine
+  Write-Host "$PWD" -ForegroundColor "Blue"
+  Write-Host "PS >" -nonewline -ForegroundColor "DarkYellow"
+
+  return " "
+}
+
+# Aliases
+
+Set-Alias vi vim
